d99d116a588cfdffb1a447f984ea6f71088c0a6d
J.M. Vishal Rashmika Perera <87840726+VishalRashmika@users.noreply.github.com>
2024-07-05 11:25:08 +0530

    build end

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@ Building process:
     //3. creating the snake
     //4. moving the snake
     //5. making the snake eat the food
-    6. making the snake grow longer
-    7. checking for collisions with edges and tail
-    8. adding title & frame
-    9. keeping the score
-    10. adding sounds  
+    //6. making the snake grow longer
+    //7. checking for collisions with edges and tail
+    //8. adding title & frame
+    //9. keeping the score
+    //10. adding sounds  
 */
 
 #include <iostream>
