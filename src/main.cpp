3095524940eb58091e06f0269c96b52965727a6d
J.M. Vishal Rashmika Perera <87840726+VishalRashmika@users.noreply.github.com>
2024-07-05 08:29:35 +0530

    base creates

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*
+Building process:
+    1. creating an blank canvas & game loop
+    2. creating the food
+    3. creating the snake
+    4. moving the snake
+    5. making the snake eat the food
+    6. making the snake grow longer
+    7. checking for collisions with edges and tail
+    8. adding title & frame
+    9. keeping the score
+    10. adding sounds  
+*/
+
+#include <iostream>
+#include <raylib.h>
+
+int main(){
+    std::cout << "Starting the game...." << std::endl;
+    return 0;
+}
